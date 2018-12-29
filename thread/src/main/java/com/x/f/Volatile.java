1a92b65e496a6fc7f8b6b76333ae027900ed572d
吴流离 <wllrx93@163.com>
2018-12-29 17:18:01 +0800

    volatile关键字

---
+++ thread/src/main/java/com/x/f/Volatile.java
@@ -0,0 +1,32 @@
+package com.x.f;
+
+/**
+ * @author zoe
+ * @date 2018-12-29
+ */
+public class Volatile{
+    public static void main(String[] args) throws Exception{
+        MyThread myThread = new MyThread();
+        new Thread(myThread,"票贩子A").start();
+        new Thread(myThread,"票贩子B").start();
+        new Thread(myThread,"票贩子C").start();
+    }
+
+}
+class MyThread implements Runnable{
+    private volatile int ticket = 10;
+    @Override
+    public void run() {
+        synchronized (this){
+            while (ticket > 0){
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName() + "卖票操作  ticket = "+ticket--);
+            }
+        }
+
+    }
+}
